3e3c968feea73c11eb56007823269ec0dd7dc747
Adam Rooney <Adam Rooney@localhost>
2015-12-14 18:29:32 +0000

    Changed stuff

--- Bootstrap4Angular/Content/js/custom.js
+++ Bootstrap4Angular/Content/js/custom.js
@@ -1,1 +1,20 @@
-﻿
+﻿var customJavaScriptHelpers = new function () {
+    // Sets a delay on how often the function passed to it can fire, pass a delay like normal and 'immediate' as well to have it fire on the leading edge of the timer rather than the trailing.
+    var debounce = function (func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+    return {
+        debounce: debounce
+    };
+}();
